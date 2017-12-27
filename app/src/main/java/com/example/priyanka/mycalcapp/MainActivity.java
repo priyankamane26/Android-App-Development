021abfe03ce8fd2ea0361d7e3902a4420c96a165
priyankamane26 <priyankamane26@gmail.com>
2017-12-27 16:50:26 -0500

    Initial commit for MyCalcApp

---
+++ app/src/main/java/com/example/priyanka/mycalcapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.priyanka.mycalcapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button addBtn = (Button) findViewById(R.id.addBtn);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
+
+                TextView resultTestView = (TextView) findViewById(R.id.resultTestView);
+
+                int num1 = Integer.parseInt(firstNumEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumEditText.getText().toString());
+
+                int result = num1 + num2;
+
+                resultTestView.setText(result+"");
+
+            }
+        });
+
+    }
+}
